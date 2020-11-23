02c27d60c72497e047e03c5dfbf99f749fec6107
[larissa bottefield] <[larissabott3@gmail.com]>
2020-11-23 14:52:06 -0600

    odd or even

--- OddOrEven/Program.cs
+++ OddOrEven/Program.cs
@@ -6,7 +6,21 @@ namespace OddOrEven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine( IsOdd(5) );
+        }
+
+        public static bool IsOdd(int num)
+        {
+            if(num % 2 == 0)
+            {
+                Console.WriteLine($"Odd");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine($"Even");
+                return false;
+            }
         }
     }
 }
